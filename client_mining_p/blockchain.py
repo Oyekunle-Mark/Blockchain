924fcc100bab4ae016fd4a6a1c7246fec0e44e90
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-10-30 14:25:40 +0100

    Removed the proof of work function

--- client_mining_p/blockchain.py
+++ client_mining_p/blockchain.py
@@ -83,25 +83,6 @@ class Blockchain(object):
     def last_block(self):
         return self.chain[-1]
 
-    def proof_of_work(self):
-        """
-        Simple Proof of Work Algorithm
-        Stringify the block and look for a proof.
-        Loop through possibilities, checking each one against `valid_proof`
-        in an effort to find a number that is a valid proof
-        :return: A valid proof for the provided block
-        """
-        # One line version of code to stringify a block
-        block_string = json.dumps(self.last_block, sort_keys=True).encode()
-
-        proof = 0
-
-        while self.valid_proof(block_string, proof) is False:
-            proof += 1
-
-        # return proof
-        return proof
-
     @staticmethod
     def valid_proof(block_string, proof):
         """
