912194ef640d6b27eebdc888c2e4f04201b31fb5
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-10-31 16:19:44 +0100

    Adds the users transaction

--- basic_wallet_p/wallet.js
+++ basic_wallet_p/wallet.js
@@ -18,6 +18,7 @@ let userId = idField.textContent;
 const idSaveButton = document.querySelector('#user-id button');
 const idInput = document.querySelector('#user-id input');
 
+// saves the id when the save button is clicked
 idSaveButton.addEventListener('click', () => {
   localStorage.setItem('id', idInput.value);
 
@@ -32,33 +33,44 @@ const createNode = element => document.createElement(element);
 const appendNode = (par, el) => par.appendChild(el);
 
 // get the transactions table
-const transactionsTable = document.querySelector('transaction table');
+const transactionsTable = document.querySelector('#transaction table');
 
+// fetch the full chain and find the users transactions
 fetch(`${URL}/chain`)
   .then(res => res.json())
   .then(data => {
-    console.log(data);
-
     const chain = data['chain'];
+
     // loop through the blocks in the chain
     for (let i = 0; i < chain.length; i++) {
       // loop through the transactions in the chain
       const transactions = chain[i]['transactions'];
+
       for (let i = 0; i < transactions.length; i++) {
         if (
           transactions[i]['sender'] === userId ||
           transactions[i]['recipient'] === userId
         ) {
+          // create tr
           const trade = createNode('tr');
+
+          // create sender td, set the value and append to tr
           const sender = createNode('td');
           sender.textContent = transactions[i]['sender'];
           appendNode(trade, sender);
+
+          // create recipient td, set the value and append to tr
           const recipient = createNode('td');
-          sender.textContent = transactions[i]['recipient'];
+          recipient.textContent = transactions[i]['recipient'];
           appendNode(trade, recipient);
+
+          // create amount td, set the value and append to tr
           const amount = createNode('td');
-          sender.textContent = transactions[i]['amount'];
+          amount.textContent = transactions[i]['amount'];
           appendNode(trade, amount);
+
+          // add tr to table
+          appendNode(transactionsTable, trade);
         }
       }
     }
